4a5708509d9043413b1a891b7d6b507bcd3160bf
Nathaniel LESCHAEVE--LEMAIRE <nathaniel.leschaevelemaire@etu.unilim.fr>
2026-01-21 21:31:46 +0100

    add of textView when condition on button click

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    // Variable to keep last added view ID
+    private var lastViewId: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,15 +25,41 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        // Récupération des vues
+        val mainLayout = findViewById<ConstraintLayout>(R.id.main)
         val premierBouton = findViewById<Button>(R.id.button_submit)
         val editText = findViewById<EditText>(R.id.editText)
         val textView = findViewById<TextView>(R.id.textView)
 
-        // Contrôleur sur le bouton
+        // Init lastViewId with button ID
+        lastViewId = premierBouton.id
+
+        // Controler on the bouton
         premierBouton.setOnClickListener {
             val text = editText.text.toString()
-            textView.text = text
+
+            if (text == "Afficher nouveau textView") {
+                val secondTextView = TextView(this)
+                secondTextView.text = text
+                secondTextView.id = View.generateViewId()
+
+                val params = ConstraintLayout.LayoutParams(
+                    ConstraintLayout.LayoutParams.WRAP_CONTENT,
+                    ConstraintLayout.LayoutParams.WRAP_CONTENT
+                )
+
+                params.topToBottom = lastViewId
+                params.startToStart = ConstraintLayout.LayoutParams.PARENT_ID
+                params.endToEnd = ConstraintLayout.LayoutParams.PARENT_ID
+                params.topMargin = 16
+
+                secondTextView.layoutParams = params
+
+                mainLayout.addView(secondTextView)
+
+                lastViewId = secondTextView.id
+            } else {
+                textView.text = text
+            }
         }
     }
 }
